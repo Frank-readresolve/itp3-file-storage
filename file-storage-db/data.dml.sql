732a28ff2023f32eb693e98ed0164405584924ae
Nibal ABAZLI <nibalabazli@nibals-macbook-pro.home>
2023-07-25 11:55:15 +0200

    create mpd

---
+++ file-storage-db/data.dml.sql
@@ -0,0 +1,6 @@
+DELETE FROM customers;
+
+INSERT INTO customers (customer_name, first_name, last_name, email, consent) VALUES 
+	('Simplon93100', 'Frank', 'MARSHALL', 'frank2@rearesolve.io',  true),
+	('SG92060', 'Lindsay', 'GRESSET', 'lindsay@gmail.com',  false),
+	('BDDFDIRE94033', 'Maroua', 'SOLTANI', 'maroua@hotmail.fr',  true);
---
+++ file-storage-db/schema.ddl.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers (
+	customer_number SERIAL PRIMARY KEY,
+	customer_name VARCHAR(100) NOT NULL UNIQUE
+	first_name VARCHAR(100) NOT NULL
+	last_name VARCHAR(100) NOT NULL
+	email VARCHAR NOT NULL
+	consent BOOLEAN
+);
