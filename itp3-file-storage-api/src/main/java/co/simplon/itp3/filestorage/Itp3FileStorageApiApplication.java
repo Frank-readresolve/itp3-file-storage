53a6081d2773b776e4325695971de1ad163117e5
Lindsay <lindsay.dev33@gmail.com>
2023-07-25 11:25:53 +0200

    first commit

---
+++ itp3-file-storage-api/src/main/java/co/simplon/itp3/filestorage/Itp3FileStorageApiApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.itp3.filestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itp3FileStorageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Itp3FileStorageApiApplication.class, args);
+	}
+
+}
