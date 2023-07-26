474b7fcf8ac7142e83878f25c028fb2c879a666d
Maroua SOLTANI <113924387+marouachd@users.noreply.github.com>
2023-07-26 15:11:51 +0200

    replace create sequence by serial

--- file-storage-db/schema.ddl.sql
+++ file-storage-db/schema.ddl.sql
@@ -1,18 +1,15 @@
 DROP TABLE IF EXISTS customers;
-DROP SEQUENCE IF EXISTS customer_number_sequence;
-
-
-CREATE SEQUENCE customer_number_sequence
-     INCREMENT BY 1
-     START WITH 10000;
+DROP SEQUENCE IF EXISTS Customers_customer_number_seq;
 
 
 CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
-    customer_number INT UNIQUE nextval('customer_number_sequence'),
+    customer_number SERIAL UNIQUE,
     customer_name VARCHAR(100) NOT NULL UNIQUE,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL,
     consent BOOLEAN
 );
+
+SELECT setval('customers_customer_number_seq', 10000, true);
