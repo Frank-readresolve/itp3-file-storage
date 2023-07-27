93d549b9741a0407c3572af6dbed72f24ade72c7
Maroua SOLTANI <113924387+marouachd@users.noreply.github.com>
2023-07-27 11:58:29 +0200

    mapping test and refactor entities and Dtos

--- itp3-file-storage-api/src/main/java/co/simplon/itp3/filestorage/services/CustomerServiceImpl.java
+++ itp3-file-storage-api/src/main/java/co/simplon/itp3/filestorage/services/CustomerServiceImpl.java
@@ -9,11 +9,13 @@ import co.simplon.itp3.filestorage.repositories.CustomerRepository;
 
 @Service
 @Transactional(readOnly = true)
-public class CustomerServiceImpl implements CustomerService {
+public class CustomerServiceImpl
+	implements CustomerService {
 
     private CustomerRepository customers;
 
-    public CustomerServiceImpl(CustomerRepository customers) {
+    public CustomerServiceImpl(
+	    CustomerRepository customers) {
 	this.customers = customers;
     }
 
@@ -21,9 +23,9 @@ public class CustomerServiceImpl implements CustomerService {
     @Transactional
     public void create(CustomerData inputs) {
 	Customer customer = new Customer();
-	customer.setCustomer_name(inputs.getCustomer_name());
-	customer.setFirst_name(inputs.getFirst_name());
-	customer.setLast_name(inputs.getLast_name());
+	customer.setCustomerName(inputs.getCustomerName());
+	customer.setFirstName(inputs.getFirstName());
+	customer.setLastName(inputs.getLastName());
 	customer.setEmail(inputs.getEmail());
 	customer.setConsent(inputs.getConsent());
 	this.customers.save(customer);
