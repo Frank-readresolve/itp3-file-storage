9e7e3428fe95714f9f8b6a64440f59b26e658b32
MilenaPC <milena771@hotmail.fr>
2023-09-25 14:07:49 +0200

    http header study

--- itp3-file-storage-api/src/main/java/co/simplon/itp3/filestorage/controllers/AnonymousFileController.java
+++ itp3-file-storage-api/src/main/java/co/simplon/itp3/filestorage/controllers/AnonymousFileController.java
@@ -1,16 +1,8 @@
 package co.simplon.itp3.filestorage.controllers;
 
-import javax.validation.Valid;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import co.simplon.itp3.filestorage.dtos.AnonymousFileData;
-import co.simplon.itp3.filestorage.dtos.FileView;
 import co.simplon.itp3.filestorage.services.AnonymousFileService;
 
 @RestController
@@ -23,12 +15,4 @@ public class AnonymousFileController {
 	    AnonymousFileService service) {
 	this.service = service;
     }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.OK)
-    public FileView upload(
-	    @ModelAttribute @Valid AnonymousFileData inputs) {
-	return service.upload(inputs);
-    }
-
 }
